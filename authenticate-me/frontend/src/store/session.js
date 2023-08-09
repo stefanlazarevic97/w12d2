c4f23a089451fd553e0dc36e0cc9a22696b69ea3
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-08 21:32:45 -0700

    add login function

---
+++ authenticate-me/frontend/src/store/session.js
@@ -0,0 +1,50 @@
+import csrfFetch from "./csrf";
+
+const SET_CURRENT_USER = 'session/setCurrentUser';
+const REMOVE_CURRENT_USER = 'session/removeCurrentUser';
+
+const setCurrentUser = user => ({
+    type: SET_CURRENT_USER,
+    payload: user
+});
+
+const removeCurrentUser = () => ({
+    type: REMOVE_CURRENT_USER
+})
+
+// const storeCurrentUser = user => {
+//     if (user) {
+//         sessionStorage.setItem("currentUser", JSON.stringify(user));
+//     } else {
+//         sessionStorage.removeItem("currentUser");
+//     }
+// }
+
+export const login = ({ credential, password }) => async dispatch => {
+    const res = await csrfFetch("/api/session", {
+        method: "POST",
+        body: JSON.stringify({ credential, password })
+    });
+
+    const data = await res.json();
+    // storeCurrentUser(data.user);
+    dispatch(setCurrentUser(data.user));
+    return res;
+}
+
+const initialState = {
+    user: JSON.parse(sessionStorage.getItem("currentUser"))
+};
+
+const sessionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_CURRENT_USER:
+            return { ...state, user: action.payload };
+        case REMOVE_CURRENT_USER:
+            return { ...state, user: null };
+        default: 
+            return state;
+    }
+};
+
+export default sessionReducer;
