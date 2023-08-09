fc72cf008b0d1682fef3611f535a14efce31ab9f
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-09 16:17:40 -0700

    add signup component

--- authenticate-me/frontend/src/App.js
+++ authenticate-me/frontend/src/App.js
@@ -1,11 +1,15 @@
 import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
 
 function App() {
     return (
         <Switch>
             <Route path="/login">
                 <LoginFormPage />
+            </Route>
+            <Route path="/signup">
+                <SignupFormPage />
             </Route>
         </Switch>
     );
