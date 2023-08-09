56a59a352e7ac3a3b9b94a54cb58ae5b43c93b9f
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-09 16:58:57 -0700

    add navigation + dropdown menu

---
+++ authenticate-me/frontend/src/components/Navigation/index.jsx
@@ -0,0 +1,33 @@
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import ProfileButton from "./ProfileButton";
+// import LoginFormModal from '../LoginFormModal';
+import './Navigation.css';
+
+export default function Navigation() {
+    const sessionUser = useSelector(state => state.session.user);
+
+    let sessionLinks;
+
+    if (sessionUser) {
+        sessionLinks = (
+            <ProfileButton user={sessionUser} />
+        );
+    } else {
+        sessionLinks = (
+            <>
+                {/* <LoginFormModal /> */}
+                <NavLink to="/signup">Sign Up</NavLink>
+            </>
+        );
+    }
+    
+    return (
+        <ul>
+            <li>
+                <NavLink exact to="/">Home</NavLink>
+            </li>
+            {sessionLinks}
+        </ul>
+    )    
+}
